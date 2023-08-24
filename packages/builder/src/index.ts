af4da5aac2864fa815db17fd17ade9e993109114
Stratulat Alexandru <alex@sandulat.com>
2023-08-24 03:20:42 +0300

    wip

--- packages/builder/src/index.ts
+++ packages/builder/src/index.ts
@@ -7,7 +7,10 @@ export type { Entity } from "./entity";
 export { createInput } from "./input";
 export type { Input } from "./input";
 
-export { SchemaValidationError as EntityValidationError, createStore } from "./store";
+export {
+  SchemaValidationError as EntityValidationError,
+  createStore,
+} from "./store";
 export type {
   Store,
   StoreData,
