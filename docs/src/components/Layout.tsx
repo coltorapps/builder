59f6dc17629af5a4899030a5124a46ad21502df0
Stratulat Alexandru <alex@sandulat.com>
2024-02-08 03:05:47 +0200

    chore: Added Twitter link to the website

--- docs/src/components/Layout.tsx
+++ docs/src/components/Layout.tsx
@@ -55,6 +55,19 @@ function Header() {
       </div>
       <div className="relative flex basis-0 justify-end gap-6 sm:gap-8 md:flex-grow">
         <Link
+          href="https://twitter.com/sandulat"
+          target="_blank"
+          className="group"
+          aria-label="Twitter (X)"
+        >
+          <svg
+            viewBox="0 0 300 300"
+            className="h-6 w-6 fill-neutral-400 group-hover:fill-neutral-500 dark:group-hover:fill-neutral-300"
+          >
+            <path d="M178.57 127.15 290.27 0h-26.46l-97.03 110.38L89.34 0H0l117.13 166.93L0 300.25h26.46l102.4-116.59 81.8 116.59h89.34M36.01 19.54H76.66l187.13 262.13h-40.66" />
+          </svg>
+        </Link>
+        <Link
           href="https://github.com/coltorapps/builder"
           target="_blank"
           className="group"
